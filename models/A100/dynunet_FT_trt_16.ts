365fbfd63c6aca9b402f9016c8146489338c5570
chezhia <chezhipower@gmail.com>
2024-09-29 15:22:29 +0000

    latest

---
+++ models/A100/dynunet_FT_trt_16.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:079a5a65757055121a7badbfd770b7b39e108e379dbf3ce3c1533e094c9b609f
+size 86090106
---
+++ models/dynunet_FT.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b7f26c66c36e6f8c05eb48ab8e61b8fef79583f0d98fa0bbfa356fef87fa6f3
+size 124185025
